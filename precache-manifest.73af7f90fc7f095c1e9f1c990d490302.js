3a7b6296bb28a47574e241399996d15daf51101a
mab1994 <boudaggamedamine1994@gmail.com>
2020-02-28 11:30:43 +0100

    Updates

---
+++ precache-manifest.73af7f90fc7f095c1e9f1c990d490302.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b9235ed9d9e16e3a5077927403848ab",
+    "url": "/mab1994/NeoNavBar.git/index.html"
+  },
+  {
+    "revision": "98eca6081c16c32a2acf",
+    "url": "/mab1994/NeoNavBar.git/static/css/main.db4cc4a5.chunk.css"
+  },
+  {
+    "revision": "fa14919ffbd75f53c9ea",
+    "url": "/mab1994/NeoNavBar.git/static/js/2.4100831a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/mab1994/NeoNavBar.git/static/js/2.4100831a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98eca6081c16c32a2acf",
+    "url": "/mab1994/NeoNavBar.git/static/js/main.e26ddd3c.chunk.js"
+  },
+  {
+    "revision": "dfb10d238b3066dbef07",
+    "url": "/mab1994/NeoNavBar.git/static/js/runtime-main.173271a3.js"
+  }
+]);
